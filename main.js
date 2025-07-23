c42d05289525877a93cc0a10b70fc0518479aa81
Paul041998 <amsterdampawloamsterdam@gmail.com>
2025-07-24 00:02:25 +0300

    main js changed

---
+++ main.js
@@ -0,0 +1,12 @@
+let myNumber = 77843;
+
+console.log(myNumber);
+
+let digit1 = 7;
+let digit2 = 7;
+let digit3 = 8;
+let digit4 = 4;
+let digit5 = 3;
+
+let result = `${digit1} ${digit2} ${digit3} ${digit4} ${digit5}`;
+console.log(result);
