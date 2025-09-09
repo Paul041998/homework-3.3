dd724b17e64b7d221a1a545d4e170c826fb83294
Paul041998 <amsterdampawloamsterdam@gmail.com>
2025-09-09 22:56:00 +0300

    changed

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ let myNumber = 77843;
 
 console.log(myNumber);
 
-let digit1 = 7;
-let digit2 = 7;
-let digit3 = 8;
-let digit4 = 4;
-let digit5 = 3;
+let digit1 = parseInt(myNumber / 10000);
+let digit2 = parseInt(myNumber / 1000) % 10;
+let digit3 = parseInt(myNumber / 100) % 10;
+let digit4 = parseInt(myNumber / 10) % 10;
+let digit5 = myNumber % 10;
 
 let result = `${digit1} ${digit2} ${digit3} ${digit4} ${digit5}`;
 console.log(result);
